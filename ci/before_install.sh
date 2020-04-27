6d6ea1f3826764a070df487d075b2bb2a0b4d309
Kneelawk <jedidiah.pommert@gmail.com>
2020-04-27 04:19:27 -0700

    Add libxss linux dependency for travis

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -2,8 +2,8 @@ set -ex
 
 main() {
   if [ $TRAVIS_OS_NAME = linux ]; then
-    sudo apt-get update
-    sudo apt-get -y install libx11-dev
+    sudo apt-get update -qq
+    sudo apt-get -y install libxss-dev
   fi
 }
 
