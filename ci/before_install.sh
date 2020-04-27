f2e90d1ba6887179bcb94e8d072146315c82bfd5
Kneelawk <jedidiah.pommert@gmail.com>
2020-04-27 05:41:50 -0700

    Add custom cross docker images with x11

---
+++ ci/before_install.sh
@@ -0,0 +1,13 @@
+set -ex
+
+CROSS_VERSION=0.2.0
+
+main() {
+  if [ $TRAVIS_OS_NAME = linux ]; then
+    # add x11 capable docker images for linux
+    docker build -t kneelawk/cross-x11:i686-unknown-linux-gnu-$CROSS_VERSION docker/i686-unknown-linux-gnu
+    docker build -t kneelawk/cross-x11:x86_64-unknown-linux-gnu-$CROSS_VERSION docker/x86_64-unknown-linux-gnu
+  fi
+}
+
+main
