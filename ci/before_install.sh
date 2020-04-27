d9cc416b74ddc995b55a6fbd4c71e72cefbd6ec3
Kneelawk <jedidiah.pommert@gmail.com>
2020-04-27 06:28:32 -0700

    Add extra custom cross docker image dependencies

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -3,9 +3,13 @@ set -ex
 CROSS_VERSION=0.2.0
 
 main() {
-  if [ $TRAVIS_OS_NAME = linux ]; then
-    # add x11 capable docker images for linux
+  # add custom docker images for linux
+
+  if [ $TARGET = i686-unknown-linux-gnu ]; then
     docker build -t kneelawk/cross-custom:i686-unknown-linux-gnu-$CROSS_VERSION docker/i686-unknown-linux-gnu
+  fi
+
+  if [ $TARGET = x86_64-unknown-linux-gnu ]; then
     docker build -t kneelawk/cross-custom:x86_64-unknown-linux-gnu-$CROSS_VERSION docker/x86_64-unknown-linux-gnu
   fi
 }
