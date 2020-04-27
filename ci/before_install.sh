6183a6983d472cdd77f4ee16ccd6fe99b7e7cc38
Kneelawk <jedidiah.pommert@gmail.com>
2020-04-27 04:36:19 -0700

    Print x11 installation info during build

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -2,8 +2,9 @@ set -ex
 
 main() {
   if [ $TRAVIS_OS_NAME = linux ]; then
-    sudo apt-get update -qq
-    sudo apt-get -y install libxss-dev
+    pkg-config --libs x11
+    dpkg -S /usr/lib/pkgconfig/x11.pc
+    dpkg -S /usr/lib/x86_64-linux-gnu/pkgconfig/x11.pc
   fi
 }
 
