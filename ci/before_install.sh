be449d35969d8361de2fe08d5c56e6a28311e1ba
Kneelawk <jedidiah.pommert@gmail.com>
2020-04-27 05:02:40 -0700

    Allow cross PKG_CONFIG_PATH passthrough

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,11 +1,7 @@
 set -ex
 
 main() {
-  if [ $TRAVIS_OS_NAME = linux ]; then
-    pkg-config --libs x11
-    dpkg -S /usr/lib/pkgconfig/x11.pc
-    dpkg -S /usr/lib/x86_64-linux-gnu/pkgconfig/x11.pc
-  fi
+  # nothing needed
 }
 
 main
