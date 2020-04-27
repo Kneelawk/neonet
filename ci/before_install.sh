ee9bf434470e90a9c5918634a6fb864ea6091c0e
Kneelawk <jedidiah.pommert@gmail.com>
2020-04-27 07:41:33 -0700

    Remove i686 target

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -4,11 +4,6 @@ CROSS_VERSION=0.2.0
 
 main() {
   # add custom docker images for linux
-
-  if [ $TARGET = i686-unknown-linux-gnu ]; then
-    docker build -t kneelawk/cross-custom:i686-unknown-linux-gnu-$CROSS_VERSION docker/i686-unknown-linux-gnu
-  fi
-
   if [ $TARGET = x86_64-unknown-linux-gnu ]; then
     docker build -t kneelawk/cross-custom:x86_64-unknown-linux-gnu-$CROSS_VERSION docker/x86_64-unknown-linux-gnu
   fi
